e100c04cc7ff2880bd4151851283681c7f483643
ΔΗΜΗΤΡΙΟΣ ΓΚΟΡΟΣ <100197395+Dimitrioz@users.noreply.github.com>
2022-03-01 19:24:14 +0200

    Name

--- Xesturgy/Xesturgy/Program.cs
+++ Xesturgy/Xesturgy/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace Xesturgy
+{
+    class Xesturgy
+    {
+    static void Main(string[] args)
+        {
+            System.Console.WriteLine("Wellcome To Xesturgy!");
+            Console.WriteLine("Please give your character a name: ");
+
+        }
+    }
+}
