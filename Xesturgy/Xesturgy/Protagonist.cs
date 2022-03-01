0b6478bb5f1260cadc1b4bea0ceeed99a0ee3fb2
vincture27 <91662922+vincture27@users.noreply.github.com>
2022-03-01 18:24:22 +0100

    Created player class

---
+++ Xesturgy/Xesturgy/Protagonist.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xesturgy
+{
+    public class Protagonist
+    {
+        public int protagonist_HP = 100;
+        public string protagonist_Name;
+    }
+}
